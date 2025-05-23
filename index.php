dbaf39500c3f2fe277b29ea787f98f960367e668
Timesh Dillon <ktimeshdilan007@gmail.com>
2025-05-23 17:54:45 +0530

    testing 1

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
-    <title>Friend Frenzy • Home</title>
+    <title>Friend Frenzy not home • Home</title>
 </head>
 <body>
     <?php
-    
+
     ?>
 </body>
 </html>
