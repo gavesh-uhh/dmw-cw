c5b2923b06ec18525c1cb87c9f040238cd75bb77
Timesh Dillon <ktimeshdilan007@gmail.com>
2025-05-23 18:53:03 +0530

    Edited header and main text of Fried Frenzy

--- index.php
+++ index.php
@@ -6,10 +6,31 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exile&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
-    <title>Friend Frenzy not home • Home</title>
+    <title>Friend Frenzy • Home</title>
 </head>
 <body>
+    <header class="header">
+
+        <a href="" class="logo">Fried <span>Frenzy</span></a>
+        <nav class="nav-btns">
+            <a href="" class="active">Login</a>
+            <a href="">Sign Up</a>
+        </nav>
+    </header>
+    <section class="main-logo-slogan">
+        <h1 class="ff">Fried Frenzy</h1>
+        <h2 class="slogan">The Ultimate Junk Food Paradise!</h2>
+    </section>
+
+
+
+
+
+
+
+
     <?php
 
     ?>
