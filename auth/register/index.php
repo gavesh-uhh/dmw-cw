c71185f5c2550b8d28f2b9e80dc981e58fb65bdd
Gavesh Saparamadu <gaveshasaparamadu06@gmail.com>
2025-05-24 10:12:07 +0530

    added. basic auth layout setup

---
+++ auth/register/index.php
@@ -0,0 +1,10 @@
+<?php
+$page_title = 'Fried Frenzy • Register';
+$page_css = '/css/register.css';
+include '../../includes/header.php';
+?>
+
+<body>
+    
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exile&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <?php if (isset($page_css)): ?>
+        <link rel="stylesheet" href="<?php echo $page_css; ?>">
+    <?php endif; ?>
+    <title><?php echo isset($page_title) ? $page_title : 'Fried Frenzy'; ?></title>
+</head>
