7333543ec9b409ec1157f055ed3bcc1ecb6b3e74
Gavesh Saparamadu <gaveshasaparamadu06@gmail.com>
2025-05-24 10:40:53 +0530

    Refactored index.php to include header and dynamic CSS, updated navigation links, and removed unused assets. Enhanced home.css with responsive styles and improved color variables in main.css.

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Exile&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="css/main.css">
     <?php if (isset($page_css)): ?>
         <link rel="stylesheet" href="<?php echo $page_css; ?>">
     <?php endif; ?>
--- index.php
+++ index.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Exile&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/home.css">
-
-    <title>Fried Frenzy • Home</title>
-</head>
+<html>
+<?php
+$page_title = 'Fried Frenzy • Home';
+$page_css = 'css/home.css';
+include 'includes/header.php';
+?>
 
 <body>
-
     <header class="header">
-        <img src="assets/header Banner.png" class="banner">
-        <a href=""><img src="assets/logo  bg .png"  class="pic" alt="logo"></a>
+        <img src="assets/logo_bg.png" id="nav_logo">
         <nav class="nav-btns">
-            <a href="">Login</a>
-            <a href="">Sign Up</a>
+            <a href="auth/login">Login</a>
+            <a href="auth/register">Sign Up</a>
         </nav>
     </header>
     <section class="main-logo-slogan">
@@ -34,19 +23,11 @@
             <h1 class="title-our-story">Our Story</h1>
             <p>Founded in 2018, FRIED FRENZY started as a late-night food blog celebrating the most iconic and
                 indulgent eats. What began as a passion project quickly grew into a global community of food
-                 lovers addicted to the crunch, the melt, and the flavor explosions that only junk food can deliver.<br>
-            <b>Today, we’re more than just a website – we’re a celebration of fast food culture,
-                a hub for foodies, and a virtual menu of cravings waiting to be satisfied.</b></p>
+                lovers addicted to the crunch, the melt, and the flavor explosions that only junk food can deliver.<br>
+                <b>Today, we're more than just a website – we're a celebration of fast food culture,
+                    a hub for foodies, and a virtual menu of cravings waiting to be satisfied.</b>
+            </p>
         </div>
     </div>
-
-
-
-
-
-    <?php
-
-    ?>
 </body>
-
 </html>
